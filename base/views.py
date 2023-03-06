d5ede979e8c50c8aa1a147afd7be797d01d166c6
ybsaatci <yagiz.saatchi@gmail.com>
2023-03-05 22:22:13 -0800

    Add render for room object

--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 # Create your views here.
+
+rooms = [
+    {'id' : 1, 'name' : 'Lets learn Python'},
+    {'id' : 2, 'name' : 'Design'},
+    {'id' : 3, 'name' : 'Front end'},
+]
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
